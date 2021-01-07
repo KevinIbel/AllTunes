321f6836b4448694e2ac21625e9720b47f6f0efe
James Angel <jra29@kent.ac.uk>
2021-01-07 19:19:33 +0000

    added docker file for client, deployment.yaml files for kubernates, build and push to script file

--- docker-buildNpush.sh
+++ docker-buildNpush.sh
@@ -5,3 +5,7 @@ docker push jamesangel/alltunes-backend
 docker build -t jamesangel/alltunes-room-service ./room-service
 
 docker push jamesangel/alltunes-room-service
+
+docker build -t jamesangel/alltunes-client ./client
+
+docker push jamesangel/alltunes-client
