a8efe8ed470432906aedf965e59d5ee9c5d96367
lowaaa <ki73@kent.ac.uk>
2021-01-17 23:38:37 +0000

    e

--- client/src/pages/userroom.jsx
+++ client/src/pages/userroom.jsx
@@ -5,37 +5,40 @@ import UserDetails from "../components/userDetails/userDetails";
 import UserSection from "../components/userSection/userSection";
 import WebPlaybackReact from "../components/spotify/webPlayback";
 import TrackTable from "../components/trackTable/TrackTable";
+import './style/room.css';
 
 
-  export default function Joinroom(props) {
-    const [playerLoaded, setPlayerLoaded] = useState(false);
-    const [access_token, setAccess_token] = useState(props.access_token);
-    const [roomKey, setRoomKey] = useState(props.roomKey);
-  
-    useState(() => {
-      setAccess_token(props.access_token);
-      setToken(props.access_token);
-    }, props.access_token);
-  
-    useState(() => {
-      setRoomKey(props.roomKey);
-    }, props.roomKey);
-    return (
-      
-      <section>
-        <div class="main">
-        <TrackTable
-        host={false}
+export default function Useroom(props) {
+  const [playerLoaded, setPlayerLoaded] = useState(false);
+  const [access_token, setAccess_token] = useState(props.access_token);
+  const [roomKey, setRoomKey] = useState(props.roomKey);
+
+  useState(() => {
+    setAccess_token(props.access_token);
+    setToken(props.access_token);
+  }, props.access_token);
+
+  useState(() => {
+    setRoomKey(props.roomKey);
+  }, props.roomKey);
+
+  return (
+    <div class="main">
+
+      <UserDetails         
+      host={true}
+        display_name={props.display_name}
+       >
+          
+        </UserDetails>
+      <br></br>
+      <TrackTable
       ></TrackTable>
       <UserSection
       host={true}
       >
       </UserSection>
 
-        </div>
-        
-      </section>
-    );
-  }
-
-
+    </div>
+  );
+}
