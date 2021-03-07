58bb9e1ae4b212ea6713e9a3aff09602f50a9280
Harrison McDonagh <hm435@kent.ac.uk>
2021-03-07 14:26:08 +0000

    Co-authored-by: Kevin Ibelgauptas <ki73@kent.ac.uk>

---
+++ client/src/components/modal/trackListModal.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+export default function QrModal(props) {
+    const url = process.env.NODE_ENV == 'development' ? "http://localhost:3000" : "http://35.246.33.106:3000"
+
+    function getModalStyle() {
+        const top = 50;
+        const left = 50;
+
+        return {
+            top: `${top}%`,
+            left: `${left}%`,
+            transform: `translate(-${top}%, -${left}%)`,
+        };
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            position: 'absolute',
+            width: 550,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }));
+
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div class="qr">
+            Let a user join the room! Scan below.<br></br><br></br>
+            <button class="buttoncss" onClick={handleOpen}
+            >Show QR Code</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div style={modalStyle} className={classes.paper}>
+                </div></Modal>
+        </div>
+    )
+}
--- client/src/pages/hostroom.jsx
+++ client/src/pages/hostroom.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { setToken } from "../dataHandler/store/actions/spotify";
 import Footer from "../components/footer/footer";
 import LobbyUsers from "../components/LobbyUsers/LobbyUsers";
@@ -6,34 +6,75 @@ import WebPlaybackReact from "../components/spotify/webPlayback";
 import TrackTable from "../components/trackTable/TrackTable";
 import QueueSection from "../components/lobbyTopTracks/queueSection";
 import "./style/hostroom.css";
+import "./style/room.css";
 import QrModal from "../components/modal/qrmodal";
+import {
+  BrowserView,
+  MobileView
+} from "react-device-detect";
+import OurLogo from './style/ourlogo.png';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
+import AudiotrackIcon from '@material-ui/icons/Audiotrack';
+import GroupIcon from '@material-ui/icons/Group';
 
 export default function Hostroom(props) {
   useState(() => {
     setToken(props.access_token);
   }, props.access_token);
 
+
   return (
-    <div class="main">
-      {props.access_token ? (
-        <WebPlaybackReact access_token={props.access_token}>
-          <Footer
-            host={true}
+    <div>
+      <BrowserView>
+        <div class="main">
+          {props.access_token ? (
+            <WebPlaybackReact access_token={props.access_token}>
+              <Footer
+                host={true}
+                roomIp={props.roomIp}
+              ></Footer>
+            </WebPlaybackReact>
+          ) : null}
+          <TrackTable
             roomIp={props.roomIp}
-          ></Footer>
-        </WebPlaybackReact>
-      ) : null}
-      <TrackTable
-        roomIp={props.roomIp}
-        host={true}
-        access_token={props.access_token}
-        roomKey={props.roomKey}
-      ></TrackTable>
-      <LobbyUsers  roomIp={props.roomIp}></LobbyUsers>
-      <QueueSection host={true} roomIp={props.roomIp}></QueueSection>
-      <QrModal roomKey={props.roomKey}></QrModal>
+            host={true}
+            access_token={props.access_token}
+            roomKey={props.roomKey}
+          ></TrackTable>
+          <LobbyUsers roomIp={props.roomIp}></LobbyUsers>
+          <QueueSection host={true} roomIp={props.roomIp}></QueueSection>
+          <QrModal roomKey={props.roomKey}></QrModal>
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div class="mobileMain">
+          <div class="logo">
+            <img class="ourLogo" src={OurLogo} alt="All Tunes Logo" />
+          </div>
+          <div class="modal">
 
+          </div>
+          <div class="switcher">
+            <button style={{background:"rgb(40, 40, 40)",borderRadius: "10px",border: "none",padding: "6px 12px"}}>
+            <QueueMusicIcon style={{ marginLeft: '-1rem' }}  fontSize={"large"}></QueueMusicIcon>
+            </button>
+            <button style={{background:"rgb(40, 40, 40)",borderRadius: "10px",border: "none",padding: "6px 12px"}}>
+            <AudiotrackIcon style={{ marginLeft: '5rem' }} fontSize={"large"}></AudiotrackIcon>
+            </button>
+            <button style={{background:"rgb(40, 40, 40)",borderRadius: "10px",border: "none",padding: "6px 12px"}}>
+            <GroupIcon style={{ marginLeft: '5rem' }} fontSize={"large"}></GroupIcon>
+            </button>
+          </div>
+          <div class="playback">
+            <WebPlaybackReact access_token={props.access_token}>
+              <Footer
+                host={true}
+                roomIp={props.roomIp}
+              ></Footer>
+            </WebPlaybackReact>
+          </div>
+        </div>
+      </MobileView>
     </div>
-
   );
 }
